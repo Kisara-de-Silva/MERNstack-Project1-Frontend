947e56a48453d67db4d9a98134b2d293e4b2be15
Kisara de Silva <manmithdesilva@gmail.com>
2025-06-20 15:02:53 +0530

    Update Users.js
    Added a users array and linked the UsersTable.

--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,25 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+const users = [
+    {
+        id: 1,
+        name: 'Perera',
+
+        id: 2,
+        name: 'Silva'
+    }
+];
 
 const Users = () => {
     return(
-        <UserForm />
-    )
+        <Box>
+            <UserForm />
+            <UsersTable rows={users} />
+        </Box>
+        
+    );
 }
 
 export default Users;
