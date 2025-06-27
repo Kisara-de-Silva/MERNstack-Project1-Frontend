83473bb7a9f539c578586be2c45b34d911b09a85
Kisara de Silva <manmithdesilva@gmail.com>
2025-06-27 15:50:32 +0530

    Update UserForm.js
    Added values and points

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,6 +1,11 @@
 import { colors, Grid, Input, Typography, Button } from "@mui/material";
+import { useState } from "react";
 
 const UserForm = props => {
+
+    const [id, setId] = useState(0);
+    const [name, setName] = useState('');
+
     return (
         <Grid 
             container
@@ -37,8 +42,8 @@ const UserForm = props => {
                     id="id"
                     name="id"
                     sx={{width: '400px'}}
-                    value={''}
-                    onChange={e => {}}
+                    value={id}
+                    onChange={e => {setId(e.target.value)}}
                 />
             </Grid>
 
@@ -60,8 +65,8 @@ const UserForm = props => {
                     id="name"
                     name="name"
                     sx={{width: '400px'}}
-                    value={''}
-                    onChange={e => {}}
+                    value={name}
+                    onChange={e =>setName(e.target.value)}
                 />
             </Grid>
 
